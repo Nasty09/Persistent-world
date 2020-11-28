0bc6cd192f290d2e751530a27a847ff42ee5ecd5
Anastasia Zaytseva <72304256+Nasty09@users.noreply.github.com>
2020-11-28 15:17:05 +0300

    Update WorldOfPlanets.cpp

--- WorldOfPlanets.cpp
+++ WorldOfPlanets.cpp
@@ -8,26 +8,27 @@ int main()
 {
     int currentPlanet = 0, newPlanet, c = 1;
     World knownWorld;
-    std::ifstream is("savegame.cereal", std::ios::binary);
-    if (is)
     {
-        char a;
-        std::cout << "Do you want restart? [y/n]\n";
-        std::cin >> a;
-        if (a == 'n')
+        std::ifstream is("savegame.cereal", std::ios::binary);
+        if (is)
         {
-            cereal::BinaryInputArchive arInput(is);
-            arInput(knownWorld);
-            is.close();
+            char a;
+            std::cout << "Do you want restart? [y/n]\n";
+            std::cin >> a;
+            if (a == 'n')
+            {
+                cereal::BinaryInputArchive arInput(is);
+                arInput(knownWorld);
+            }
+            else
+            {
+                knownWorld.add(Planet());
+            }
         }
         else
         {
             knownWorld.add(Planet());
         }
-    }
-    else
-    {
-        knownWorld.add(Planet());
     }
     std::cout << "You are on the Planet:\n";
     knownWorld.getWorld()[currentPlanet].OutputInfo();
